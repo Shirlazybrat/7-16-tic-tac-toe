08945087cb8042b1c6e3630f7978486f09c5ffdf
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-07-21 17:00:20 -0400

    more elements added  to project for UX

--- script.js
+++ script.js
@@ -1,14 +1,101 @@
 var whosTurn = 1; //Start off on pleyer ons turn
 
+var winners = [
+	["A1", "A2", "A3"], //row1
+	["B1","B2","B3"], //row2
+	["C1","C2","C3"], //row3
+	["A1", "B2", "C3"], //diag1
+	["A1","B1","C1"], //col1
+	["A2","B2", "C2"], //col2
+	["A3","B3","C3"], //col3
+	["A3","B2","C1"], //other diag
+];
+
+var player1 =[];  //Array where we will stash the squares player has checked
+var player2 = [];  // array for player 2
+var someoneWon = false;
+
+
+function whoPlays(){
+document.getElementById('play1').addEventListener("click", function(){
+		alert("You will play aginst the computer.");});
+
+
+document.getElementById('play2').addEventListener("click", function(){
+		var person1 = prompt("Enter the name of P1ayer 1");
+		var person2 = prompt("Enter the name of P1ayer 2")
+		document.getElementById('name1');
+		document.getElementById('name2');
+		name1.innerHTML = "Player 1 - " + person1;
+		name2.innerHTML = "Player 2 - " + person2;
+	});
+
+	if(document.getElementById('play1').addEventListener("click")){
+		document.getElementById(play2).className += ' hidden';
+	}
+}
+
+whoPlays(); //call the function
+
 
 function markSquare(square){
-	if(whosTurn == 1){
-		square.innerHTML ='X';
-		whosTurn = 2
+	if(someoneWon){
+		console.log("Someone already won");
 	}
-	else {
-		square.innerHTML = 'O';
-		whosTurn = 1;
+
+	//check to see if this square is in either player array,
+	//if so, goodbye
+	else if((player1.indexOf(square.id) == -1) //Lok in player 1 array for the square
+		&& (player2.indexOf(square.id) == -1)){ // look in player 2 array for the square
+			//If both are -1, then it's neither array
+
+		if(whosTurn == 1){
+			square.innerHTML ='X';
+			whosTurn = 2;
+			player1.push(square.id);
+			checkWin(player1,1);
+		}
+		else {
+			square.innerHTML = 'O';
+			whosTurn = 1;
+			player2.push(square.id);
+			checkWin(player2,2);
+		}
 	}
-	console.log(square);
+	else{
+		console.log("Something is already there!!! No cheating!!");
+	}
 }
+
+
+function checkWin(currentPlayersSquares, whoJustMarked){
+	var rowCount = 0;
+	// loop through the outter array
+	for(var i = 0; i < winners.length; i++){
+		//loop through the inner loop
+		rowCount = 0;
+		for(var j = 0; j < winners[i].length; j++){
+			if(currentPlayersSquares.indexOf(winners[i][j]) > -1){
+				//Hit!!
+				rowCount++;
+			}
+			if(rowCount == 3){
+				///BINGO!!
+				gameOver(whoJustMarked, winners[i]);
+			}
+		}
+	}
+}
+
+
+function gameOver(whoWon, winningCombo){
+	document.getElementById('message');
+	message.innerHTML = "Congratulations Player " + whoWon + 
+	", You Won!! You won with  " + winningCombo.join(', ');
+
+	for(var i = 0; i < winningCombo.length; i++){
+		document.getElementById(winningCombo[i]).className += ' winner';
+	}
+	someoneWon = true;
+}
+
